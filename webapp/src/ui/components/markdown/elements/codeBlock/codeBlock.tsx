ca5f03097e90f83d79cd9bcbb608138bd684e9a7
Cameron Campbell <cameron.campbell325@gmail.com>
2025-06-14 21:24:31 +0100

    fixed issue with inline code being offset from the rest of the text. code block language highlighting is dynamically loaded and now supports much more languages.

--- webapp/src/ui/components/markdown/elements/codeBlock/codeBlock.tsx
+++ webapp/src/ui/components/markdown/elements/codeBlock/codeBlock.tsx
@@ -1,5 +1,5 @@
-import { useMemo } from "react"
-import { codeToHtml, createHighlighter } from 'shiki'
+import { useEffect, useMemo, useState } from "react"
+import { codeToHtml, createHighlighter, type BundledLanguage, type BundledTheme, type HighlighterGeneric } from 'shiki'
 
 import { Squircle } from "@/ui/components"
 
@@ -7,24 +7,41 @@ import styles from "./codeBlock.module.scss"
 
 const highlighter = await createHighlighter({
     themes: ["github-dark-default"],
-    langs: ["ts", "js", "css", "lua", "luau", "jsx", "toml", "json"],
-    langAlias: {
-        "luau": "lua"
-    }
+    langs: [],
 })
 
+const shikiLangExists = (highlighter: HighlighterGeneric<BundledLanguage, BundledTheme>, lang: string) => {
+    try { highlighter.getLanguage(lang) } catch { return false }
+    return true
+}
+
 export const CodeBlock = ({
-    className, children
+    className, children, ...rest
 }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>) => {
     const lang = useMemo(() => /language-(\w+)/.exec(className || '')?.[1], [])
 
     if (typeof children === "string") {
+        const [ code, setCode ] = useState(highlighter.codeToHtml(children, {
+            lang: (lang && shikiLangExists(highlighter, lang) && lang || undefined) as any,
+            theme: "github-dark-default",
+            tabindex: false,
+            ...rest
+        }))
+
         try {
-            let code = highlighter.codeToHtml(children, {
-                lang: lang as any,
-                theme: "github-dark-default",
-                tabindex: false
-            })
+            useEffect(() => {
+                (async () => {
+                    try {
+                        await highlighter.loadLanguage(lang as any)
+                    } catch {}
+
+                    setCode(highlighter.codeToHtml(children, {
+                        lang: lang ?? "plaintext",
+                        theme: "github-dark-default",
+                        tabindex: false
+                    }))
+                })()
+            }, [])
 
             return <Squircle cornerRadius={8} className={[ className, styles.codeBlock ].join(" ")} as="code">
                 <Squircle className={styles.border} cornerRadius={8} as="code" />
--- webapp/src/ui/components/markdown/elements/inlineCode/inlineCode.tsx
+++ webapp/src/ui/components/markdown/elements/inlineCode/inlineCode.tsx
@@ -6,7 +6,6 @@ export const InlineCode = ({
     className, children
 }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>) => {
     return <Squircle cornerRadius={8} className={[ className, styles.inlineCode ].join(" ")} as="code">
-        <Squircle as="code" className={styles.border} id="hehehehe" cornerRadius={8} />
         {children}
     </Squircle>
 }
